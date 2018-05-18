cb314c17a1565e9f20acbcaea151fcd9abeeb627
Tom Chinery <tom@sparxsdigital.co.uk>
2018-05-18 14:27:21 +0100

    Fix errors again

--- ClusteredMapView.js
+++ ClusteredMapView.js
@@ -109,10 +109,12 @@ export default class ClusteredMapView extends PureComponent {
     // //////////////////////////////////////////////////////////////////////////////////
     // get cluster children
     const children = this.index.getLeaves(cluster.properties.cluster_id, this.props.clusterPressMaxChildren),
-          markers = children;
+          markers = children
 
     // fit right around them, considering edge padding
-    this.mapview.fitToCoordinates(markers.map(m => { latitude: parseFloat(m.geometry.coordinates.latitude), longitude: parseFloat(m.geometry.coordinates.longitude)}), { edgePadding: this.props.edgePadding });
+      this.mapview.fitToCoordinates(markers.map((m) => {
+          return { latitude: m.geometry.coordinates[0], longitude: m.geometry.coordinates[1] }
+      }), { edgePadding: this.props.edgePadding })
 
     this.props.onClusterPress && this.props.onClusterPress(cluster.properties.cluster_id, markers)
   }
